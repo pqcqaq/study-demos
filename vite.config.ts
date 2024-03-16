c684bb651a2691fd6fd3c250f88d5c1b92ce0bff
pqcqaq <905739777@qq.com>
2024-03-16 18:41:00 +0800

    查看编译结果插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +14,7 @@ export default defineConfig({
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-    vue()],
+    vue(),
+    Inspect(),
+  ],
 })
